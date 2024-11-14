454c405195e5e18e37403a8aeff75cdebe9376b7
Ali Naqvi <naqvia13@mcmaster.ca>
2024-11-14 14:01:14 -0500

    Add input voltage/current acquisition function prototypes

--- Core/Inc/MCP3221.h
+++ Core/Inc/MCP3221.h
@@ -14,32 +14,25 @@
 
 #define MCP3221_DEVICE_ID		0x4D 				// According to VIPER backplane schematic
 
-#define MCP3221_RESOLUTION  	4096     			// 12-bit resolution
-#define MCP3221_I2C_TIMEOUT 	HAL_MAX_DELAY
-#define MCP3221_VREF_MV         3000				// in mV (is 3V according to VIPER backplane)
-
-typedef enum {
-    MCP3221_SMOOTHING_NONE = 0,
-    MCP3221_SMOOTHING_EMA,
-    MCP3221_SMOOTHING_ROLLING_AVG
-} MCP3221_Smoothing;
+#define MCP3221_RESOLUTION  	4095     			// 12-bit resolution
+#define MCP3221_VREF_MV         3300				// in mV (is 3.3V according to VIPER backplane)
+#define MCP3221_DATA_BYTES		2
 
 // define constructor
 typedef struct {
     I2C_HandleTypeDef *hi2c;  						// Pointer to the I2C handle
-    uint8_t devAddr; 								// I2C address of the MCP3221 (7-bit address)
-    MCP3221_SmoothingMethod smoothingMethod;
-    uint16_t alpha;           						// For exponential moving average (1-256)
-    uint8_t numSamples;       						// Number of samples for rolling average
-    uint16_t samples[20];     						// Sample buffer for rolling average
-    uint16_t emAvg;
+    uint8_t i2c_Addr; 								// I2C address of the MCP3221 (7-bit address)
 } MCP3221;
 
-HAL_StatusTypeDef MCP3221_Init(MCP3221 *dev, I2C_HandleTypeDef *hi2c, uint8_t devAddr);
+HAL_StatusTypeDef MCP3221_Init(MCP3221 *dev, I2C_HandleTypeDef *hi2c, uint8_t i2c_Addr);
 
-HAL_StatusTypeDef MCP3221_Ping(MCP3221 *dev); 		// error handling
+uint16_t MCP3221_readADC(MCP3221 *dev);
 
-//HAL_StatusTypeDef MCP3221_ReadRawData(MCP3221 *dev, uint16_t *rawData);
+float MCP3221_getADCVoltage(uint16_t adc_value);
+
+float MCP3221_getInputVoltage(uint16_t adc_value);
+
+float MCP3221_getInputCurrent(uint16_t adc_value, float sense_res);
 
 #endif
 
